635a48ac50f3b3729987ac592c12408c15597f63
Camille Galladjov <ujinjinjin@outlook.com>
2020-02-02 17:14:53 +0300

    ignoring upm-preparator folder

--- meta_generator.py
+++ meta_generator.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
     script_path_items.append('templates')
     templates_folder = '/'.join(script_path_items)
 
-    ignored_paths = [r'.+\.meta', r'\.git/.+']
+    ignored_paths = [r'.+\.meta', r'\.git/.+', r'.*/?upm-preparator.*']
     paths_dict = _get_file_paths('**', ignored_paths)
     templates = _get_meta_templates(templates_folder)
 
